b451274197f7b7e5a0dfcabde789b82461f909f9
Swardiantara Silalahi <35374847+swardy1221@users.noreply.github.com>
2022-04-24 09:22:26 +0700

    nambah simple_transformer

---
+++ dataset/preprocessing.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+
+dataset_path = '/content/drive/MyDrive/Colab Notebooks/ner-drone/test_candidate2.csv'
+dataset = pd.read_csv(dataset_path, encoding= 'unicode_escape')
+
+print("Hello World")
